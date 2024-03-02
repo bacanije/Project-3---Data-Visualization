d32d00f8f0657c872292d5e4554755b8e6b49460
stephanielabelle <stephanie.labelle14@gmail.com>
2024-03-02 14:47:28 -0500

    working with flask app

--- Flask/application/routes.py
+++ Flask/application/routes.py
@@ -8,7 +8,19 @@ import plotly.express as px
 
 @app.route("/")
 def home():
-    return render_template("index.html", title = "Home")
+
+    df= pd.DataFrame({"Game": ["Game1", "Game2", "Game3", "Game4"],
+                 "Canada": [4,5,6,7],
+                 "EU": [6,3,4,9],
+                  "Japan": [2,5,4,6]})
+    fig = px.bar(df, x="Game", y=["Canada","EU", "Japan"],
+             height=400)
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    return render_template("index.html", graphJSON = graphJSON, title="home")
+
+    
 
 @app.route("/overview")
 def overview():
